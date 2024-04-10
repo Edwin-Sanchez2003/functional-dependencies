1efb862a246f8121ea03018dff5de37d37c8bf99
Edwin Sanchez <edwsanch@iu.edu>
2024-04-10 19:17:52 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+    Functional Dependencies
+    Edwin Sanchez
+    
+    This program allows you to automatically
+    find functional dependencies in a given
+    set of data. This is a bottom-up approach
+    to database design.
+"""
+
+import os
+import csv
+import json
+
+import argparse
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
